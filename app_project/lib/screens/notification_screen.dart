35b5a504f74314dd35ab530ae38acf8db161f334
Duong Tung <19522494@gm.uit.edu.vn>
2021-10-06 07:29:35 +0700

    hotfix/change_folder_name

--- app_project/lib/screens/notification_screen.dart
+++ app_project/lib/screens/notification_screen.dart
@@ -2,9 +2,6 @@
 import 'package:firebase_core/firebase_core.dart';*/
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:test_fix/providers/user.dart';
-import 'package:test_fix/Helper/this_is_me.dart';
 import 'package:test_fix/widgets/notification_item.dart';
 
 class NotificationScreen extends StatelessWidget {
@@ -26,29 +23,30 @@ class NotificationScreen extends StatelessWidget {
             );
           }
           return snapshot.data!.docs.length > 0
-              ? */Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      margin: EdgeInsets.only(top: 10, left: 10),
-                      height: 25,
-                      child: Text(
-                        'TODAY',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.black54),
-                      ),
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        itemBuilder: (context, index) => NotificationItem(
-                          factorId: "???", //snapshot.data!.docs[index]['factorId'],
-                          username: "???"//snapshot.data!.docs[index]['content'],
-                          //imageUrl: snapshot.data!.docs[index]['imageUrl'],
-                        ),
-                        itemCount: 4 //snapshot.data!.docs.length,
-                      ),
-                    ),
-                   /* Container(
+              ? */
+        Column(
+      children: [
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsets.only(top: 10, left: 10),
+          height: 25,
+          child: const Text(
+            'TODAY',
+            style:
+                TextStyle(fontWeight: FontWeight.bold, color: Colors.black54),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemBuilder: (context, index) => NotificationItem(
+                  factorId: "???", //snapshot.data!.docs[index]['factorId'],
+                  username: "???" //snapshot.data!.docs[index]['content'],
+                  //imageUrl: snapshot.data!.docs[index]['imageUrl'],
+                  ),
+              itemCount: 4 //snapshot.data!.docs.length,
+              ),
+        ),
+        /* Container(
                       width: double.infinity,
                       margin: EdgeInsets.only(top: 10, left: 10),
                       height: 25,
@@ -68,12 +66,12 @@ class NotificationScreen extends StatelessWidget {
                         itemCount: snapshot.data!.docs.length,
                       ),
                     ),*/
-                  ],
-                );
-             /* : const Center(
+      ],
+    );
+    /* : const Center(
                   child: Text("You dont have any notis"),
                 );*/
-     /*   },
+    /*   },
       ),
     );*/
   }
